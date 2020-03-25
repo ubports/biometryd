9aa9587a1434879416a482ab453d579468cd9d44
Erfan Abdi <erfangplus@gmail.com>
2020-03-25 20:20:43 +0430

    Complete android part of api

--- android/hybris/biometry_fp_hidl_for_hybris.cpp
+++ android/hybris/biometry_fp_hidl_for_hybris.cpp
@@ -98,6 +98,36 @@ struct BiometricsFingerprintClientCallback : public IBiometricsFingerprintClient
     Return<void> onEnumerate(uint64_t deviceId, uint32_t fingerId,
                              uint32_t groupId, uint32_t remaining) override;
 };
+    
+UHardwareBiometryFingerprintAcquiredInfo HIDLToUFingerprintAcquiredInfo(FingerprintAcquiredInfo info) {
+    switch(info) {
+        case FingerprintAcquiredInfo::ACQUIRED_GOOD: return ACQUIRED_GOOD;
+        case FingerprintAcquiredInfo::ACQUIRED_PARTIAL: return ACQUIRED_PARTIAL;
+        case FingerprintAcquiredInfo::ACQUIRED_INSUFFICIENT: return ACQUIRED_INSUFFICIENT;
+        case FingerprintAcquiredInfo::ACQUIRED_IMAGER_DIRTY: return ACQUIRED_IMAGER_DIRTY;
+        case FingerprintAcquiredInfo::ACQUIRED_TOO_SLOW: return ACQUIRED_TOO_SLOW;
+        case FingerprintAcquiredInfo::ACQUIRED_TOO_FAST: return ACQUIRED_TOO_FAST;
+        case FingerprintAcquiredInfo::ACQUIRED_VENDOR: return ACQUIRED_VENDOR;
+        default:
+            return ACQUIRED_GOOD;
+    }
+}
+    
+UHardwareBiometryFingerprintError HIDLToUFingerprintError(FingerprintError error) {
+    switch(error) {
+        case FingerprintError::ERROR_NO_ERROR: return ERROR_NO_ERROR;
+        case FingerprintError::ERROR_HW_UNAVAILABLE: return ERROR_HW_UNAVAILABLE;
+        case FingerprintError::ERROR_UNABLE_TO_PROCESS: return ERROR_UNABLE_TO_PROCESS;
+        case FingerprintError::ERROR_TIMEOUT: return ERROR_TIMEOUT;
+        case FingerprintError::ERROR_NO_SPACE: return ERROR_NO_SPACE;
+        case FingerprintError::ERROR_CANCELED: return ERROR_CANCELED;
+        case FingerprintError::ERROR_UNABLE_TO_REMOVE: return ERROR_UNABLE_TO_REMOVE;
+        case FingerprintError::ERROR_LOCKOUT: return ERROR_LOCKOUT;
+        case FingerprintError::ERROR_VENDOR: return ERROR_VENDOR;
+        default:
+            return ERROR_NO_ERROR;
+    }
+}
 
 Return<void> BiometricsFingerprintClientCallback::onEnrollResult(uint64_t deviceId,
     uint32_t fingerId, uint32_t groupId, uint32_t remaining)
@@ -108,8 +138,46 @@ Return<void> BiometricsFingerprintClientCallback::onEnrollResult(uint64_t device
     return Void();
 }
 
+Return<void> BiometricsFingerprintClientCallback::onAcquired(uint64_t deviceId, FingerprintAcquiredInfo acquiredInfo,
+    int32_t vendorCode)
+{
+    if (hybris_fp_instance_cb && hybris_fp_instance_cb->acquired_cb) {
+        hybris_fp_instance_cb->acquired_cb(deviceId, HIDLToUFingerprintAcquiredInfo(acquiredInfo), vendorCode, hybris_fp_instance_cb->context);
+    }
+    return Void();
+}
 
-//TODO: Add more
+Return<void> BiometricsFingerprintClientCallback::onAuthenticated(uint64_t deviceId, uint32_t fingerId, uint32_t groupId, const hidl_vec<uint8_t>& token)
+{
+    if (hybris_fp_instance_cb && hybris_fp_instance_cb->authenticated_cb) {
+        hybris_fp_instance_cb->authenticated_cb(deviceId, fingerId, groupId, hybris_fp_instance_cb->context);
+    }
+    return Void();
+}
+
+Return<void> BiometricsFingerprintClientCallback::onError(uint64_t deviceId, FingerprintError error, int32_t vendorCode)
+{
+    if (hybris_fp_instance_cb && hybris_fp_instance_cb->error_cb) {
+        hybris_fp_instance_cb->error_cb(deviceId, HIDLToUFingerprintError(error), vendorCode, hybris_fp_instance_cb->context);
+    }
+    return Void();
+}
+
+Return<void> BiometricsFingerprintClientCallback::onRemoved(uint64_t deviceId, uint32_t fingerId, uint32_t groupId, uint32_t remaining)
+{
+    if (hybris_fp_instance_cb && hybris_fp_instance_cb->removed_cb) {
+        hybris_fp_instance_cb->removed_cb(deviceId, fingerId, groupId, remaining, hybris_fp_instance_cb->context);
+    }
+    return Void();
+}
+
+Return<void> BiometricsFingerprintClientCallback::onEnumerate(uint64_t deviceId, uint32_t fingerId, uint32_t groupId, uint32_t remaining)
+{
+    if (hybris_fp_instance_cb && hybris_fp_instance_cb->enumerate_cb) {
+        hybris_fp_instance_cb->enumerate_cb(deviceId, fingerId, groupId, remaining, hybris_fp_instance_cb->context);
+    }
+    return Void();
+}
 
 }
 
@@ -135,6 +203,26 @@ UHardwareBiometryCallback_::UHardwareBiometryCallback_(UHardwareBiometryParams* 
       context(params->context)
 {
 
+}
+
+UHardwareBiometryRequestStatus HIDLToURequestStatus(RequestStatus req) {
+    switch(req) {
+        case RequestStatus::SYS_UNKNOWN: return SYS_UNKNOWN;
+        case RequestStatus::SYS_OK: return SYS_OK;
+        case RequestStatus::SYS_ENOENT: return SYS_ENOENT;
+        case RequestStatus::SYS_EINTR: return SYS_EINTR;
+        case RequestStatus::SYS_EIO: return SYS_EIO;
+        case RequestStatus::SYS_EAGAIN: return SYS_EAGAIN;
+        case RequestStatus::SYS_ENOMEM: return SYS_ENOMEM;
+        case RequestStatus::SYS_EACCES: return SYS_EACCES;
+        case RequestStatus::SYS_EFAULT: return SYS_EFAULT;
+        case RequestStatus::SYS_EBUSY: return SYS_EBUSY;
+        case RequestStatus::SYS_EINVAL: return SYS_EINVAL;
+        case RequestStatus::SYS_ENOSPC: return SYS_ENOSPC;
+        case RequestStatus::SYS_ETIMEDOUT: return SYS_ETIMEDOUT;
+        default:
+            return SYS_UNKNOWN;
+    }
 }
 
 bool UHardwareBiometry_::init()
@@ -171,7 +259,85 @@ uint64_t UHardwareBiometry_::preEnroll()
     return fpHal->preEnroll();
 }
 
-//TODO: Add more
+UHardwareBiometryRequestStatus UHardwareBiometry_::enroll(uint8_t *hat, uint32_t gid, uint32_t timeoutSec)
+{
+    if (fpHal == nullptr) {
+        ALOGE("Unable to get FP service\n");
+        return 0;
+    }
+    
+    return HIDLToURequestStatus(fpHal->enroll(hat, gid, timeoutSec));
+}
+
+UHardwareBiometryRequestStatus UHardwareBiometry_::postEnroll()
+{
+    if (fpHal == nullptr) {
+        ALOGE("Unable to get FP service\n");
+        return 0;
+    }
+    
+    return HIDLToURequestStatus(fpHal->postEnroll());
+}
+
+uint64_t UHardwareBiometry_::getAuthenticatorId()
+{
+    if (fpHal == nullptr) {
+        ALOGE("Unable to get FP service\n");
+        return 0;
+    }
+    
+    return fpHal->getAuthenticatorId();
+}
+
+UHardwareBiometryRequestStatus UHardwareBiometry_::cancel()
+{
+    if (fpHal == nullptr) {
+        ALOGE("Unable to get FP service\n");
+        return 0;
+    }
+    
+    return HIDLToURequestStatus(fpHal->cancel());
+}
+
+UHardwareBiometryRequestStatus UHardwareBiometry_::enumerate()
+{
+    if (fpHal == nullptr) {
+        ALOGE("Unable to get FP service\n");
+        return 0;
+    }
+    
+    return HIDLToURequestStatus(fpHal->enumerate());
+}
+
+UHardwareBiometryRequestStatus UHardwareBiometry_::remove(uint32_t gid, uint32_t fid)
+{
+    if (fpHal == nullptr) {
+        ALOGE("Unable to get FP service\n");
+        return 0;
+    }
+    
+    return HIDLToURequestStatus(fpHal->remove(gid, fid));
+}
+
+UHardwareBiometryRequestStatus UHardwareBiometry_::setActiveGroup(uint32_t gid, char *storePath)
+{
+    if (fpHal == nullptr) {
+        ALOGE("Unable to get FP service\n");
+        return 0;
+    }
+    
+    return HIDLToURequestStatus(fpHal->setActiveGroup(gid, storePath));
+}
+
+UHardwareBiometryRequestStatus UHardwareBiometry_::authenticate(uint64_t operationId, uint32_t gid)
+{
+    if (fpHal == nullptr) {
+        ALOGE("Unable to get FP service\n");
+        return 0;
+    }
+    
+    return HIDLToURequestStatus(fpHal->authenticate(operationId, gid));
+}
 
 /////////////////////////////////////////////////////////////////////
 // Implementation of the C API
@@ -215,4 +381,42 @@ uint64_t u_hardware_biometry_preEnroll(UHardwareBiometry self)
     return self->preEnroll();
 }
 
-//TODO: Add more
+UHardwareBiometryRequestStatus u_hardware_biometry_enroll(UHardwareBiometry self, uint8_t *hat, uint32_t gid, uint32_t timeoutSec)
+{
+    return self->enroll(hat, gid, timeoutSec);
+}
+
+UHardwareBiometryRequestStatus u_hardware_biometry_postEnroll(UHardwareBiometry self)
+{
+    return self->postEnroll();
+}
+
+uint64_t u_hardware_biometry_getAuthenticatorId(UHardwareBiometry self)
+{
+    return self->getAuthenticatorId();
+}
+
+UHardwareBiometryRequestStatus u_hardware_biometry_cancel(UHardwareBiometry self)
+{
+    return self->cancel();
+}
+
+UHardwareBiometryRequestStatus u_hardware_biometry_enumerate(UHardwareBiometry self)
+{
+    return self->enumerate();
+}
+
+UHardwareBiometryRequestStatus u_hardware_biometry_remove(UHardwareBiometry self, uint32_t gid, uint32_t fid)
+{
+    return self->remove(gid, fid);
+}
+
+UHardwareBiometryRequestStatus u_hardware_biometry_setActiveGroup(UHardwareBiometry self, uint32_t gid, char *storePath)
+{
+    return self->setActiveGroup(gid, storePath);
+}
+
+UHardwareBiometryRequestStatus u_hardware_biometry_authenticate(UHardwareBiometry self, uint64_t operationId, uint32_t gid)
+{
+    return self->authenticate(operationId, gid);
+}
