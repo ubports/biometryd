ad032c9bfed8b1ac9773553987c338ca0acda008
Erfan Abdi <erfangplus@gmail.com>
2020-05-12 13:03:30 +0430

    We’re UBports foundation

--- android/hybris/biometry_fp_hidl_for_hybris.cpp
+++ android/hybris/biometry_fp_hidl_for_hybris.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020 Canonical Ltd.
+ * Copyright © 2020 UBports foundation Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3 as
