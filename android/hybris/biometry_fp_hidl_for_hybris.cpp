0e087e38804f42e3ca9fa8447ceae71b7d1cac68
Marius Gripsgard <marius@ubports.com>
2021-02-09 02:29:44 +0100

    Fix Copyright

--- android/hybris/biometry_fp_hidl_for_hybris.cpp
+++ android/hybris/biometry_fp_hidl_for_hybris.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020 UBports foundation Ltd.
+ * Copyright © 2020 UBports foundation.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3 as
--- src/biometry/devices/android.cpp
+++ src/biometry/devices/android.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 UBports foundation, Ltd.
+ * Copyright (C) 2020 UBports foundation.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
